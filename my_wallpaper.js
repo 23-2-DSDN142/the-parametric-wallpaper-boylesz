20c7d93bb23b80c659b612ba05fc3e21b15d1d8b
boylesz <63565717+boylesz@users.noreply.github.com>
2023-07-24 10:41:07 +1200

    Added glyph to wallpaper

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,23 +1,36 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let flower_width  = 50;
+let flower_height = 50;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(147, 217, 219); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  fill(245, 125, 215);
+  ellipse(100, 100, flower_width * 0.75, flower_height * 3)
+  ellipse(100, 100, flower_width * 3, flower_height * 0.75)
+
+  fill(242, 255, 59);
+  push();
+  translate(100, 100);
+  rotate(45);
+  ellipse(0, 0, flower_width * 0.75, flower_height * 3)
+  ellipse(0, 0, flower_width * 3, flower_height * 0.75)
+  pop();
+
+  fill(255, 254, 230);
+  ellipse(100, 100, flower_width, flower_height);
 }
